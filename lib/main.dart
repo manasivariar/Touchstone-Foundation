4a2dcce208ebeeaea735ddc8282342286ebf2328
Manasi Variar <manasirvariar13121999@gmail.com>
2021-10-23 23:48:04 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Touchstone Foundation',
+      title: 'MANASI 21RDFPP',
       home: ControlPanel(),
     );
   }
